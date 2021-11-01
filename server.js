9059c058f64f8bc2b567e220f260a794c871116a
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-01 13:49:18 +1100

    added script

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
 app.use('/api', api);
+app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
