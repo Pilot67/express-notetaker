df1914031469738cce884a59acadd181868b6a39
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-03 12:56:43 +1100

    added file promises

--- routes/notes.js
+++ routes/notes.js
@@ -1,24 +1,19 @@
-const fs = require('fs');
+const fspromises = require('fs/promises');
 const util = require('util');
 const {v4: uuid} = require('uuid');
 const notes = require('express').Router();
-const readFromFile = util.promisify(fs.readFile);
-const writeToFile = util.promisify(fs.writeFile);
-
-// const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-
+const {readFromFile} = require('../utils/frs')
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
   console.info(`${req.method} request received for notes`);
-  readFromFile('./db/db.json','utf8')
+  fspromises.readFile('./db/db.json','utf8')
   .then((data) => res.json(JSON.parse(data)))
   .catch((error) => console.log(error));
 });
-
 // POST route to save a new note
 notes.post('/', (req, res) => {
     if(req.body) {
-        readFromFile('./db/db.json','utf8')
+        fspromises.readFile('./db/db.json','utf8')
         .then((data) => {
             const {title, text} = req.body;
             const newNote = {
@@ -26,10 +21,9 @@ notes.post('/', (req, res) => {
                 text,
                 id: uuid()
             }
-            console.log(newNote);
             const parsedData = JSON.parse(data);
             parsedData.push(newNote);
-            writeToFile('./db/db.json', JSON.stringify(parsedData, null, 4))
+            fspromises.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4))
         .then(() => {
             console.log("success writing to file")
             res.json(`new data saved to note database`);
@@ -44,16 +38,15 @@ notes.post('/', (req, res) => {
     });    
     }
 });
-
+// DELETE route to delete a note
 notes.delete('/:noteId',(req, res) => {
     const noteId = req.params.noteId;
     console.log("DeleteNote request")
-    console.log(noteId)
-    readFromFile('./db/db.json','utf8')
+    fspromises.readFile('./db/db.json','utf8')
     .then((data) => JSON.parse(data))
     .then((data) => {
         const newArr = data.filter((delNote) => delNote.id !== noteId)
-        writeToFile('./db/db.json', JSON.stringify(newArr, null, 4))
+        fspromises.writeFile('./db/db.json', JSON.stringify(newArr, null, 4))
         .then(() => {
             console.log("success writing new file")
             res.json(`Note deleted from note database`);
@@ -65,8 +58,7 @@ notes.delete('/:noteId',(req, res) => {
     })
     .catch((error) => {
         console.log(error);
-    });    
-
+    });
 });
 
 module.exports = notes;
